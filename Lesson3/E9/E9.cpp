3ee1147e56f500bf268565ae76cc4887b125bcc7
Francesco Balassone <77972200+balassone@users.noreply.github.com>
2022-03-31 19:02:48 +0200

    FINALMENTE

--- Lesson3/E9/E9.cpp
+++ Lesson3/E9/E9.cpp
@@ -5,19 +5,12 @@ int main(){
 
 	for (int i{0}; i<9; ++i){
 
-		cout << "#";
-
-		for(int j{0}; j<7; ++j){
-			if(i==0 || i==8){
-				cout << "#";
-			}
-			else {
-				cout << "*";
-			}
+		for (int j{0}; j<9; ++j){
+			if(i==0 || i==8 || j==0 || j==8){cout << "#";}
+			else if((i<5 && (i>=1+j || i>=9-j)) || (i>4 && (i<8-j || i<j))){cout << " ";}
+			else cout << "*";
 		}
-
-		cout << "#"<<endl;
-
+		cout << endl;
 	}
 
 	return 0;
