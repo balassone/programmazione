87954289172c168cc8f46d03dbcf7a9806c27f59
Francesco Balassone <77972200+balassone@users.noreply.github.com>
2022-04-12 15:54:05 +0200

    Fixed LCS2

--- Lesson6/LCS2/LCS2.cpp
+++ Lesson6/LCS2/LCS2.cpp
@@ -1,69 +1,54 @@
 #include <iostream>
-#include <array>
 #include <string>
-using std::array;
+#include <array>
+
 using std::cout;
 using std::endl;
+using std::array;
 using std::string;
 
-const size_t col{8};
+template <typename T>
+T max(const T& a, const T& b){
+	return (a<=b) ? b : a;
+}
 const size_t row{9};
+const size_t col{8};
 
-void printArray(const array<array<int, col>, row>& A);
-
-void LCS(array<array<int, col>, row>& A, string, string);
-
+void printMat(const array<array<int, col>,row>&);
+void lcs(array<array<int, col>, row>&, const string&, const string&);
 int main(){
-	string p{"ATACCAGA"}, s{"CTCCTAG"};
+	string s{"ATACCAGA"}, t{"CTCCTAG"};
 	
-	
-
-	array<array<int, col>, row> myarr;
-	for(unsigned int i{0} ; i<row; ++i){
-		for(unsigned int j{0}; j<col; ++j){
-			myarr[i][j]=0;
-		}
-	}
-
-	LCS(myarr,p,s);
-	printArray(myarr);
+	array <array<int, col>, row> myArray{};
+	lcs(myArray, s, t);
+	printMat(myArray);
 	return 0;
 }
 
-void LCS(array<array<int, col>,row>& A, string p, string s){
-	for(unsigned int i{0}; i<row; ++i){
-		int count{0};
-		for(unsigned int j{0}; j<col; ++j){
-			A[i][j]=count;
-			if (i==0 || j==0){
-				A[i][j]=0;
-			}
-			else{
-				if(p[i-1]==s[j-1]){
-					A[i][j]=++count;
-				}
-				else if(A[i-1][j-1]>A[i][j]){
-					A[i][j]=A[i-1][j-1];
-				}
-				else if(A[i][j-1]>A[i][j]){
-					A[i][j]=A[i][j-1];
-				}
-				else if(A[i-1][j]>A[i][j]){
-					A[i][j]=A[i-1][j];
-				}
-			}
-		}
-	}
+void printMat(const array<array<int, col>, row>& a){
 
-}
-
-void printArray(const array<array<int,col>,row>& A){
-
-	for (auto row : A){
-		for(auto elem : row){
-			cout << elem << "\t";
+	for (const auto& row : a){
+		for (const auto& elem : row){
+			cout << elem << " ";
 		}
 		cout << endl;
 	}
 
+}
+
+void lcs(array<array<int, col>, row>& A,const string& s,const string& t){
+
+
+	for(unsigned int i{0}; i<row; ++i){
+		for(unsigned int j{0}; j<col; ++j){
+			if (i==0 || j==0){
+				A[i][j]=0;
+			}	
+			else if(s[i-1]==t[j-1]){
+				A[i][j]=1+A[i-1][j-1];
+			}
+			else
+				A[i][j]=max(A[i-1][j], A[i][j-1]);
+		}
+	}
 }
