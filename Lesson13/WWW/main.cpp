102772054c0766c646a30393f611ef85f88956c3
Francesco Balassone <77972200+balassone@users.noreply.github.com>
2022-05-20 12:32:05 +0200

    Rivedi rec comunque

--- Lesson13/WWW/main.cpp
+++ Lesson13/WWW/main.cpp
@@ -6,5 +6,14 @@ using std::cout;
 int main(){
 	WWW w(6);
 	w.printRanks();
+	std::cout << "Updating ranks ..." << std::endl;
+	w.addLink(0,3);
+	w.addLink(0,5);
+	w.addLink(1,2);
+	w.addLink(2,0);
+	w.addLink(2,1);
+	w.addLink(2,3);
+	w.pageRank();
+	w.printRanks();
 	return 0;
 }
