b035163d8a2099fc225dec0c22b1c352d4768b23
Francesco Balassone <77972200+balassone@users.noreply.github.com>
2022-07-17 12:52:04 +0200

    Fix

--- Algorithm/Heap Priority Queue/HeapInsert.h
+++ Algorithm/Heap Priority Queue/HeapInsert.h
@@ -3,12 +3,15 @@
 #include <vector>
 template <typename T>
 //heap insert with vector
-void HeapInsert(std::vector<T>& A, const T& x){
-    int i=A.size()-1;
-    while(i>0 && A[i]>A[(i-1)/2]){
-        A[i]=A[(i-1)/2];
-        i=(i-1)/2;
+void heapInsert(std::vector<T>& heap, T elem){
+    heap.push_back(elem);
+    int i = heap.size()-1;
+    while(i>0 && heap[i]>heap[(i-1)/2]){
+        T temp = heap[i];
+        heap[i] = heap[(i-1)/2];
+        heap[(i-1)/2] = temp;
+        
+        i = (i-1)/2;
     }
-    A[i]=x;
 }
 #endif
